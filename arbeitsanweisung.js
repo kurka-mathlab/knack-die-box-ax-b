91d3c76eb496c90c28efb0fc8ec8c5f547fc0f17
kurka-mathlab <kisselsabine@googlemail.com>
2026-04-29 23:12:39 +0200

    Update arbeitsanweisung.js
    Leerzeile entfernt

--- arbeitsanweisung.js
+++ arbeitsanweisung.js
@@ -1,5 +1,4 @@
-const instructionText = `
-Wie viele Streichhölzer befinden sich in einer Box?
+const instructionText = `Wie viele Streichhölzer befinden sich in einer Box?
 Tippe auf Streichhölzer oder Boxen, um sie abzulegen.
 Markiere unten ein Teil und tippe dann links oder rechts, um es zurückzulegen.
 `;
