401b561ca04e1dcf0c4b3a629e0dfdb625845b07
kurka-mathlab <kisselsabine@googlemail.com>
2026-04-29 23:00:37 +0200

    Update aufgaben.js
    Aufgaben geändert

--- aufgaben.js
+++ aufgaben.js
@@ -5,9 +5,9 @@
 // rightMatches = Streichhölzer rechts
 
 const aufgaben = {
-  1: { leftBoxes: 1, leftMatches: 3, rightBoxes: 0, rightMatches: 7 },
-  2: { leftBoxes: 2, leftMatches: 1, rightBoxes: 0, rightMatches: 9 },
-  3: { leftBoxes: 3, leftMatches: 2, rightBoxes: 1, rightMatches: 10 },
-  4: { leftBoxes: 2, leftMatches: 4, rightBoxes: 0, rightMatches: 12 },
-  5: { leftBoxes: 4, leftMatches: 0, rightBoxes: 2, rightMatches: 8 }
+  1: { leftBoxes: 1, leftMatches: 2, rightBoxes: 0, rightMatches: 6 },
+  2: { leftBoxes: 2, leftMatches: 0, rightBoxes: 0, rightMatches: 6 },
+  3: { leftBoxes: 1, leftMatches: 1, rightBoxes: 0, rightMatches: 10 },
+  4: { leftBoxes: 5, leftMatches: 0, rightBoxes: 0, rightMatches: 15 },
+  5: { leftBoxes: 3, leftMatches: 0, rightBoxes: 0, rightMatches: 18 }
 };
