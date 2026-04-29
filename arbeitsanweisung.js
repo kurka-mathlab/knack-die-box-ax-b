4d6cf7e13c7766d12b9f9ca85dff04db1e4dd107
kurka-mathlab <kisselsabine@googlemail.com>
2026-04-29 22:14:57 +0200

    Add files via upload

---
+++ arbeitsanweisung.js
@@ -0,0 +1,2 @@
+const instructionText = `Wie viele Streichhölzer befinden sich in einer Box?
+Ziehe auf beiden Seiten gleich viel weg!`;
---
+++ aufgaben.js
@@ -0,0 +1,7 @@
+const aufgaben = {
+  1: { leftBoxes: 1, leftMatches: 3, rightBoxes: 0, rightMatches: 7 },
+  2: { leftBoxes: 2, leftMatches: 1, rightBoxes: 0, rightMatches: 9 },
+  3: { leftBoxes: 3, leftMatches: 2, rightBoxes: 1, rightMatches: 10 },
+  4: { leftBoxes: 2, leftMatches: 4, rightBoxes: 0, rightMatches: 12 },
+  5: { leftBoxes: 4, leftMatches: 0, rightBoxes: 2, rightMatches: 8 }
+};
