87d4925dd3a4fa2e428d843d3c9a828fa48d80b8
kurka-mathlab <kisselsabine@googlemail.com>
2026-05-01 22:54:24 +0200

    Update arbeitsanweisung.js

--- arbeitsanweisung.js
+++ arbeitsanweisung.js
@@ -1,3 +1,3 @@
-const instructionText = `Finde heraus, <strong>wie viele Streichhölzer in einer Schachtel sein müssen.</strong><br>
-Jeweils links und rechts im Kasten sollen immer <strong>genau gleich viele</strong> Hölzer sein – bedenke dabei, dass sich in den Schachteln auch Hölzer befinden.<br>
+const instructionText = `Finde heraus, <strong>wie viele Streichhölzer in einer Schachtel sein müssen.</strong>
+Jeweils links und rechts im Kasten sollen immer <strong>genau gleich viele</strong> Hölzer sein – bedenke dabei, dass sich in den Schachteln auch Hölzer befinden.
 Tippe auf <strong>Streichhölzer oder Boxen</strong>, um sie abzulegen.`;
