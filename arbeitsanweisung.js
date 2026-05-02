c402e68ae70d4db33ed370eab42ddd6957a0530c
kurka-mathlab <kisselsabine@googlemail.com>
2026-05-02 21:50:34 +0200

    Änderung der Arbeitsanweisung

--- arbeitsanweisung.js
+++ arbeitsanweisung.js
@@ -1,3 +1,3 @@
-const instructionText = `Finde heraus, <strong>wie viele Streichhölzer in einer Schachtel</strong> sein müssen.
-Jeweils links und rechts im Kasten sollen immer <strong>genau gleich viele Hölzer</strong> sein – bedenke dabei, dass sich in den Schachteln auch Hölzer befinden.
-<strong>Tippe auf</strong> Streichhölzer oder Boxen, um sie abzulegen.`;
+const instructionText = `Vor dir auf dem virtuellen Tisch liegen Streichhölzer und Streichholzschachteln. Der Tisch ist durch eine schwarze Trennwand in zwei Hälften geteilt. <strong>Auf beiden Seiten der Trennwand</strong> befinden sich jeweils <strong>die gleiche Anzahl an Streichhölzern</strong>, manche Streichhölzer sind in den Schachteln versteckt (in jeder Schachtel sind gleich viele Hölzchen). 
+<strong>Finde heraus, wie viele Streichhölzer in einer Schachtel sein müssen.</strong> 
+<strong>Tippe auf die Streichhölzer und Schachteln, um sie abzulegen.`;
